bd14e4603614e1a3daf055af95ceeb158eaa8e19
efann <efann@beowurks.com>
2020-12-11 15:10:14 -0600

    Re-added server module.

--- server/src/test/java/com/beoearth/server/ServerApplicationTests.java
+++ server/src/test/java/com/beoearth/server/ServerApplicationTests.java
@@ -1,3 +1,11 @@
+/*
+ * BeoEarth
+ * Copyright(c) 2009-2020, Beowurks
+ * Original Author: Eddie Fann
+ * License: Eclipse Public License - v 2.0 (https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html)
+ *
+ */
+
 package com.beoearth.server;
 
 import org.junit.jupiter.api.Test;
