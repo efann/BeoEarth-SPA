f7f69e18a15fe58c1225c5cac5bdeaf3e4238859
efann <efann@beowurks.com>
2020-12-11 15:12:54 -0600

    Part of re-added server module.

---
+++ server/src/main/java/com/beoearth/server/ServletInitializer.java
@@ -0,0 +1,31 @@
+/*
+ * BeoEarth
+ * Copyright(c) 2009-2020, Beowurks
+ * Original Author: Eddie Fann
+ * License: Eclipse Public License - v 2.0 (https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html)
+ *
+ */
+
+package com.beoearth.server;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+public class ServletInitializer extends SpringBootServletInitializer
+{
+
+  // ---------------------------------------------------------------------------------------------------------------------
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+  {
+    return (application.sources(ServerApplication.class));
+  }
+  // ---------------------------------------------------------------------------------------------------------------------
+
+}
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
